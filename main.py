442cb43b929994e914b811bed921ac62c00375c5
Maxwell <130581484+Maxw311nyimbili@users.noreply.github.com>
2023-08-04 22:09:32 +0000

    render the titles

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
+
 
 with col1:
     st.image("images/IMG-0089.jpg", width=300)
@@ -14,3 +15,22 @@ with col2:
     Web Development skills.
     """
     st.info(content)
+
+with st.container():
+    st.write("Below you can find some of the apps I built in Python. Feel free to contact me!")
+
+# to get information from the data.csv file, we can use a built-in module called pandas that is best suited
+# with dealing with csv files
+
+df = pandas.read_csv("data.csv", sep=";")  # to get the information in form of a table, use the sep=";" as the text is
+# separated by a semicolon.
+
+col3, col4 = st.columns(2)
+with col3:
+    # this will iterate through the first
+    for index, item in df[:10].iterrows():
+        st.header(item["title"])
+
+with col4:
+    for index, item in df[10:].iterrows():
+        st.header(item["title"])
